1bc85c974673b1049fbde37e55fd51edec76994a
sashinpivotal <sashin@pivotal.io>
2018-11-19 19:45:47 -0500

    secound assignment

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController1.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class WelcomeController1 {
+
+    private final String message;
+
+    public WelcomeController1(@Value("${welcome.message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -1,6 +1,6 @@
 package test.pivotal.pal.tracker;
 
-import io.pivotal.pal.tracker.WelcomeController;
+import io.pivotal.pal.tracker.WelcomeController1;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +9,7 @@ public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
-        WelcomeController controller = new WelcomeController("A welcome message");
+        WelcomeController1 controller = new WelcomeController1("A welcome message");
 
         assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
